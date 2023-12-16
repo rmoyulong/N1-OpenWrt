dfa860b3f973e25253b6df0292366e729b0874ad
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2023-12-16 10:45:41 +0800

    设置底包的路径

--- Scripts/quick.sh
+++ Scripts/quick.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #设置rootfs.tar.gz路径
-echo "OWRT_PATH=https://github.com/rmoyulong/N1-OpenWrt/releases/download/OpenWrt_s912_23.12.15_11.26.16/openwrt-armvirt-64-generic-rootfs.tar.gz" >> $GITHUB_ENV
+OWRT_PATH=https://github.com/rmoyulong/N1-OpenWrt/releases/download/OpenWrt_s912_23.12.15_11.26.16/openwrt-armvirt-64-generic-rootfs.tar.gz
