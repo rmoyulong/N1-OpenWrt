e3d6b4c39eefd22423543189abe265441fc6af03
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2023-12-16 11:08:18 +0800

    模块优化：1、打包基础底包模块 2、利用底包转换为需要的固件模块

--- Scripts/quick.sh
+++ Scripts/quick.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-#设置rootfs.tar.gz路径
+#设置rootfs.tar.gz路径并下载
 OWRT_PATH="https://github.com/rmoyulong/N1-OpenWrt/releases/download/OpenWrt_s912_23.12.15_11.26.16/openwrt-armvirt-64-generic-rootfs.tar.gz"
+curl -L -o output/openwrt-armvirt-64-generic-rootfs.tar.gz ${{ OWRT_PATH }}
