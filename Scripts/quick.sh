fff80a1e51477a7f9d2a2ea9ab6f262396feebe9
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2023-12-18 12:42:24 +0800

    Delete Scripts/quick.sh

--- Scripts/quick.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#设置rootfs.tar.gz路径并下载
-OWRT_PATH="https://github.com/rmoyulong/ophub-OpenWrt/releases/download/OpenWrt_s912_23.12.17_12.32.44/openwrt-armvirt-64-generic-rootfs.tar.gz"
-curl -L -o output/openwrt-armvirt-64-generic-rootfs.tar.gz $OWRT_PATH
