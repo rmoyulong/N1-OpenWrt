783a5e2b319b11703c90a5a0b2fce3a95a7e582c
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-01-08 14:20:53 +0800

    将斐讯N1和章鱼星球的底包路径分开设置

--- Scripts/N1.sh
+++ Scripts/N1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #设置斐讯N1的rootfs.tar.gz路径并下载
-OWRT_PATH="https://github.com/rmoyulong/N1-OpenWrt/releases/download/OpenWrt_s905d_23.12.18_01.30.14/openwrt-armvirt-64-generic-rootfs.tar.gz"
+OWRT_PATH="https://github.com/rmoyulong/rootfs_builder/releases/download/OpenWrt_s905d_24.01.07_19.31.16/openwrt-armvirt-64-generic-rootfs.tar.gz"
 curl -L -o output/openwrt-armvirt-64-generic-rootfs.tar.gz $OWRT_PATH
