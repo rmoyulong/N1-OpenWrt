728ddd2e10ac247b369339e2d8a5e0f3334ad099
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-01-19 23:06:01 +0800

    将斐讯N1和章鱼星球的底包路径分开设置

--- Scripts/N1.sh
+++ Scripts/N1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #设置斐讯N1的rootfs.tar.gz路径并下载
-OWRT_PATH="https://github.com/rmoyulong/OneCloud_OpenWrt/releases/download/OpenWrt_immortalwrt_24.01.17_02.36.52/immortalwrt-meson-meson8b-thunder-onecloud-rootfs.tar.gz"
+OWRT_PATH="https://github.com/rmoyulong/rootfs_builder/releases/download/OpenWrt_s905d_24.01.19_20.46.09/immortalwrt-armsr-armv8-generic-rootfs.tar.gz"
 curl -L -o output/openwrt-armvirt-64-generic-rootfs.tar.gz $OWRT_PATH
