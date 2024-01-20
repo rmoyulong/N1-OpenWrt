35e012a7dd7f828778c0ce0317b7dfa34e6c689a
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-01-21 05:14:57 +0800

    将斐讯N1和章鱼星球的底包路径分开设置

--- Scripts/zyxq.sh
+++ Scripts/zyxq.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #设置章鱼星球的rootfs.tar.gz路径并下载
-OWRT_PATH="https://github.com/rmoyulong/rootfs_builder/releases/download/OpenWrt_s912_24.01.08_16.02.03/openwrt-armvirt-64-generic-rootfs.tar.gz"
+OWRT_PATH="https://github.com/rmoyulong/rootfs_builder/releases/download/OpenWrt_s912_24.01.21_03.01.36/immortalwrt-armsr-armv8-generic-rootfs.tar.gz"
 curl -L -o output/openwrt-armvirt-64-generic-rootfs.tar.gz $OWRT_PATH
